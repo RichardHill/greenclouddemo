30ce275835622080aa02c92ac1282abbfb874cd0
Richard Hill <RichardHill@users.noreply.github.com>
2021-01-05 17:11:06 +0000

    Update handler.js
    Demo test

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@
  * Very simple Lambda which just returns a string on invocation.
  */
 module.exports.handler = function(event, context) {
-    context.succeed('Hello, World!');   
+    context.succeed('Hello, Green Cloud !!');   
     console.log("The event is -: " + JSON.stringify(event));
     console.log("The context is -: " + JSON.stringify(context)); 
 };
